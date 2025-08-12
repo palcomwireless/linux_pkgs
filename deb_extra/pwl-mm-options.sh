2318673acb40d5acfff0e0e6791c7a17edd3c15c
palcomwireless <162967689+palcomwireless@users.noreply.github.com>
2025-08-12 18:53:45 +0800

    MM version dependency check for mm options usage

--- deb_extra/pwl-mm-options.sh
+++ deb_extra/pwl-mm-options.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+MM_VERSION=$(ModemManager --version | head -n 1 | awk '{print $2}')
+
+mm_version_ge() {
+    [ "$(printf '%s\n' "$1" "$2" | sort -V | head -n1)" = "$2" ]
+}
+
 mm_options=('--test-low-power-suspend-resume' '--test-quick-suspend-resume')
+mm_base_option=('--test-quick-suspend-resume')
 device=`lspci -D -nn | grep 14c0:4d75`
 
 usb_mm_options=('--test-quick-suspend-resume')
@@ -28,7 +35,11 @@ else
     options_to_use=()
 
     if [ -n "$device" ]; then
-        options_to_use=("${mm_options[@]}")
+        if mm_version_ge "$MM_VERSION" "1.24.2"; then
+            options_to_use=("${mm_options[@]}")
+        else
+            options_to_use=("${mm_base_option[@]}")
+        fi
     fi
     if [ -n "$usb_device" ]; then
         options_to_use=("${usb_mm_options[@]}")
